6edac4f9726e96302bcbbe5c427cb38585500c00
Bret Morris <bret@useallfive.com>
2014-06-23 14:08:04 -0700

    Remove authentication.

--- application/views.py
+++ application/views.py
@@ -5,28 +5,6 @@ from flask import request
 from flask import Response
 import logging
 
-def check_auth(username, password):
-    """This function is called to check if a username /
-    password combination is valid.
-    """
-    return username == '' and password == ''
-
-def authenticate():
-    """Sends a 401 response that enables basic auth"""
-    return Response(
-    'Could not verify your access level for that URL.\n'
-    'You have to login with proper credentials', 401,
-    {'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
-            return authenticate()
-        return f(*args, **kwargs)
-    return decorated
-
 def main(path):
     if "PhantomJS" in request.headers.get('User-Agent'):
         return render_template('homepage.html')
@@ -51,7 +29,6 @@ def main(path):
 
     return render_home(path)
 
-@requires_auth
 def render_home(path):
     return render_template('homepage.html')
 
