d39393d73de46f1db331c5b7cb11bfe3ca20aa3c
Justin Anastos <janastos@useallfive.com>
2014-07-22 17:26:54 -0400

    Remove unncessary comments in exampleView.js.

--- web/js/apps/example/exampleView.js
+++ web/js/apps/example/exampleView.js
@@ -4,10 +4,8 @@ define([
     'hbars!apps/example/templates/exampleItem'
 ], function(App, layoutTemplate, itemTemplate) {
     App.module('ExampleApp.View', function(View, App, Backbone, Marionette, $, _) {
-        // Extend the Marionette LayoutView
+
         View.Layout = Marionette.LayoutView.extend({
-            // Define regions for the layout. These regions need to exist in
-            // the DOM
             regions: {
                 bodyRegion: '.body-region'
             },
@@ -16,12 +14,11 @@ define([
 
             template: layoutTemplate
         });
-        // Extend the Marionette ItemView
+
         View.Item = Marionette.ItemView.extend({
-            // Use the itemTemplate
             template: itemTemplate
         });
     });
-    // Return the View definition
+
     return App.ExampleApp.View;
 });
